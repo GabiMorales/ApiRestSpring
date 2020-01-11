5ba260e06a2e7d83bdaafdf3ddda268432fc716c
GabiMorales <gabrimorales381@gmail.com>
2020-01-10 23:50:37 -0300

    Add files via upload

---
+++ api-rest/src/main/java/com/init/product/entities/Product.java
@@ -0,0 +1,35 @@
+package com.init.product.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class Product {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="nombre", nullable=false, length=30)
+	private String nombre;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	} 
+	
+}
